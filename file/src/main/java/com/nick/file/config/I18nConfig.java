47b7326e9d45e0a1d9a0c4035e9f82c7d94ab48e
beastmouth <hbj@33.cn>
2018-09-04 11:52:07 +0800

    加入swagger插件

--- file/src/main/java/com/nick/file/config/I18nConfig.java
+++ file/src/main/java/com/nick/file/config/I18nConfig.java
@@ -3,6 +3,7 @@ package com.nick.file.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 
@@ -17,5 +18,21 @@ public class I18nConfig extends WebMvcConfigurationSupport {
     @Bean
     public LocaleResolver localeResolver(){
         return new MyLocaleResolver();
+    }
+
+    /**
+     * 发现如果继承了WebMvcConfigurationSupport，则在yml中配置的相关内容会失效。 需要重新指定静态资源
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 }
